53dca6fe9e6f3cfec1ee4f1e399e73125a174149
Lucas Penney <lucaspenney@gmail.com>
2012-09-22 19:38:27 -0700

    Designed map (map.js array)

--- game.js
+++ game.js
@@ -107,4 +107,11 @@ function handleInteractions() {
 	}
 }
 
+$("#canvas").click(function(e){
+   var parentOffset = $(this).parent().offset(); 
+   //or $(this).offset(); if you really just want the current element's offset
+   var relX = e.pageX - parentOffset.left;
+   var relY = e.pageY - parentOffset.top;
+   console.log('Mouse Click in canvas')
+});
 
